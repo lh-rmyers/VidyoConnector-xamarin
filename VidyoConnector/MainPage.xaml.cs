3ff6a7ffd58426696ac29a3cf2af6a4f9b7fc89a
Stephen Lorello <slorello@Vidyo-LT0839-M.attlocal.net>
2018-09-07 17:09:55 -0400

    Revert "added icon and splash page. Fixed bug where preview would not work in VS for mac"
    This reverts commit c12583fcd77e27e0af496549dcb90cd4330dea15.

--- VidyoConnector/MainPage.xaml.cs
+++ VidyoConnector/MainPage.xaml.cs
@@ -111,17 +111,20 @@ namespace VidyoConnector
             }
         }
 
+        // For some reason, when OnSizeAllocated method is contained, the UI preview in MainPage.xaml.cs is not shown.
+        // Set #if to false if you want to see a valid preview of the UI.
+#if true
+        // Handle orientation change
         protected override void OnSizeAllocated(double width, double height)
         {
             base.OnSizeAllocated(width, height);
 
-            if (Math.Abs(width - _pageWidth) > 0.001 || Math.Abs(height - _pageHeight) > 0.001)
-            {
+            if (Math.Abs(width - _pageWidth) > 0.001 || Math.Abs(height - _pageHeight) > 0.001) {
                 _pageWidth = width;
                 _pageHeight = height;
-                if (_vidyoController != null)
-                    _vidyoController.OnOrientationChange();
+                _vidyoController.OnOrientationChange();
             }
         }
+#endif
     }
 }
