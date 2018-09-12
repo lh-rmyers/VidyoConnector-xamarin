78fae0b269b5fd7d632483c77782ce36a2cbd73a
slorelloVidyo <slorello@vidyo.com>
2018-09-12 17:32:58 -0400

    Splash Screen, app Icon, and fixing IOS rendering preview issue

--- VidyoConnector/MainPage.xaml.cs
+++ VidyoConnector/MainPage.xaml.cs
@@ -111,10 +111,6 @@ namespace VidyoConnector
             }
         }
 
-        // For some reason, when OnSizeAllocated method is contained, the UI preview in MainPage.xaml.cs is not shown.
-        // Set #if to false if you want to see a valid preview of the UI.
-#if true
-        // Handle orientation change
         protected override void OnSizeAllocated(double width, double height)
         {
             base.OnSizeAllocated(width, height);
@@ -122,9 +118,9 @@ namespace VidyoConnector
             if (Math.Abs(width - _pageWidth) > 0.001 || Math.Abs(height - _pageHeight) > 0.001) {
                 _pageWidth = width;
                 _pageHeight = height;
-                _vidyoController.OnOrientationChange();
+                if (_vidyoController != null)
+                    _vidyoController.OnOrientationChange();
             }
         }
-#endif
     }
 }
